e167c63c094ea384d21d24a4e0c1ed0857c4520a
kamil-87 <k.kamil87-87@yandex.ru>
2020-12-09 00:29:47 +0500

    search on the page

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,5 +1,31 @@
 <template>
-<div class="search">
-
+<div class="container-fluid search mt20 border1 p10">
+  <input type="text" @input="searchText=$event.target.value" class="border1">
+  <p>Всего имен: {{users.length}}</p>
+  <p>Совпадений: {{getFilteredNames.length}}</p>
+  <ul class="mt20">
+    <li
+      class="user-list fgrey"
+      v-for="(user, i) in getFilteredNames"
+      :key="i"
+    >
+      {{user}}
+    </li>
+  </ul>
 </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    searchText: '',
+    users: ['Камиль', 'Ринат', 'Виталий', 'Динис', 'Данил', 'Дим'],
+  }),
+
+  computed: {
+    getFilteredNames() {
+      return this.users.filter(user => user.toLowerCase().includes(this.searchText.toLowerCase()))
+    }
+  }
+}
+</script>
